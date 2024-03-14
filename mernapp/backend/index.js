86127b4f82f3acc00f145c0709c333999d5d3020
Jai Pandey <128478851+DarthYeager@users.noreply.github.com>
2024-03-14 20:52:58 +0530

    Update index.js

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -4,7 +4,7 @@ const port = 5000
 const mongoDB = require("./db")
 mongoDB();
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://taste-hunt.vercel.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
