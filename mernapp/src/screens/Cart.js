820f1dcb431a02d124a68146d99c0f9ed6d5db01
Jai Pandey <128478851+DarthYeager@users.noreply.github.com>
2024-03-15 10:45:00 +0530

    Update Cart.js

--- mernapp/src/screens/Cart.js
+++ mernapp/src/screens/Cart.js
@@ -13,7 +13,7 @@ export default function Cart(){
     }
     const handleCheckOut = async()=>{
         let userEmail = localStorage.getItem("userEmail");
-        let response = await fetch("https://my-app-name.vercel.com/api/orderData"),{
+        let response = await fetch("https://mernapp.vercel.com/api/orderData"),{
             method: 'POST',
             headers:{
                 'Content-Type': 'application/json'
